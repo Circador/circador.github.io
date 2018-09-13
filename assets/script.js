1340730a6914c08146c248dd184e5714141db2ac
Circador <ericyou00@yahoo.com>
2018-09-13 12:00:56 -0400

    fixed town data format issues

--- assets/script.js
+++ assets/script.js
@@ -119,9 +119,9 @@ function calculateSelectedData(){
 
     var diffDict = {};
     for (var j = 0; j < sortedPrecinctData.length; j++){
-      diffDict[sortedPrecinctData[j].key] = (sortedPrecinctData[j].value/curPrecTotal);
+      diffDict[sortedPrecinctData[j].key] = (+sortedPrecinctData[j].value/curPrecTotal);
     }
-    curProp.precinctData.support_diff = diffDict;
+    curProp.precinctData.support_diff = +diffDict;
 
     // ***************** UPDATE TOWN LEVEL DATA *******************
     var curTownTotal = curProp.townData.total_counted;
